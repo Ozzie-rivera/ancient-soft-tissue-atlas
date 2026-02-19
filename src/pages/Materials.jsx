95ad84c7b6451c3255dd05b227a9b0350fba21dc
Matthew Cserhati <csmatyi1@gmail.com>
2026-02-18 20:49:48 -0800

    Materials
    Updated materials page a little bit 260218

--- src/pages/Materials.jsx
+++ src/pages/Materials.jsx
@@ -6,7 +6,12 @@ function Materials() {
             <h1>Materials & Methods</h1>
             <section>
                 <h2>Materials</h2>
-                <p>Many of the samples were kindly provided by Dr. Mark Armitage.</p>
+                <p>The images in this database come from three main sources.<br/> 
+                    The first is a collection of images made by James Solliday in house, many of which used samples provided by Mark Armitage. These samples were made using bright-field illumination, crossed polarized light, differential interference contrast, and phase contrast microscopic techniques.<br/>
+                    The second collection of images comes from manually or semi-automatically extracting images from a set of 130 peer-reviewed publications on ancient soft tissue. These papers are listed on the Publications page of the Database. Some images in the database are cross-linked to the corresponding publication in PubMed.
+                    The third collection of images comes from third parties that have collected and annotated their own images of ancient soft tissues.<br/>
+                    These images and their annotations are constantly being added to the database.
+                </p>
             </section>
 
             <section>
