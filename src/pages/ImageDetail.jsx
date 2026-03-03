e27ca298e5ef1fbb629bc4ca3abe6e5defb8ac0f
Matthew Cserhati <csmatyi1@gmail.com>
2026-03-02 21:34:15 -0800

    260302
    Added article 122, made new link

--- src/pages/ImageDetail.jsx
+++ src/pages/ImageDetail.jsx
@@ -19,17 +19,29 @@ function ImageDetail() {
     }
 
     const imageUrl = `/newimages/${item.filename}`;
+    const httpattern = `http.*`;
 
     return (
         <div style={{ padding: 24 }}>
             <Link to="/explore">Back to Explore</Link>
             <h1>{item.taxon}</h1>
             <p><strong>Tissue:</strong> {item.tissue} &nbsp; | &nbsp; <strong>Interval:</strong> {item.interval}
-            {item.reference && (
+            {item.reference && item.reference !== "Unpublished" && (
+                item.reference.includes("http") ? (
                 <>
                 &nbsp; | &nbsp;
-                <a href={`https://pubmed.ncbi.nlm.nih.gov/${item.reference}/`} target="_blank" rel="noopener noreferrer">Read on PubMed</a>
+                <a href={item.reference} target="_blank" rel="noopener noreferrer">
+                    View on Article Website
+                </a>
                 </>
+                ) : (
+                <>
+                &nbsp; | &nbsp;
+                <a href={`https://pubmed.ncbi.nlm.nih.gov/${item.reference}/`} target="_blank" rel="noopener noreferrer">
+                    View on PubMed
+                </a>
+                </>
+                )
             )}
             </p>
 
