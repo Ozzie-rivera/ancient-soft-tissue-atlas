e0c14dc3262ef082a24cb84f4723ebbf08ce22d2
Matthew Cserhati <csmatyi1@gmail.com>
2026-03-07 22:56:48 -0800

    260307
    Added images carousel

---
+++ src/components/ImageCarousel.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function ImageCarousel() {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  };
+
+  return (
+    <div style={{ width: "600px", margin: "auto" }}>
+      <Slider {...settings}>
+        <div>
+          <img src="/public/newimages/00016.jpg" width="100%" />
+        </div>
+        <div>
+          <img src="/public/newimages/00063.jpg" width="100%" />
+        </div>
+        <div>
+          <img src="/public/newimages/00134.jpg" width="100%" />
+        </div>
+        <div>
+          <img src="/public/newimages/00262.jpg" width="100%" />
+        </div>
+      </Slider>
+    </div>
+  );
+}
+
+export default ImageCarousel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
 import "./Home.css"
+import React from "react";
+import ImageCarousel from "../components/ImageCarousel";
 function Home() {
     return (
         <div className="home-container">
-            <img src="public/newimages/00016.jpg" width="500" height="300"/>
+            <ImageCarousel />
             <h1>What is ASTA?</h1>
             <p>The Ancient Soft Tissue Atlas (ASTA) is a digital collection of microscopic images from ancient vertebrate organisms, including fish, amphibians, and dinosaurs such as <i>Triceratops</i> and <i>Tyrannosaurus rex</i>.
                Our goal is to organize and share images of bone cells, red blood cells, collagen, and other tissues—along with their scientific details—so researchers and students can explore this unique field of study.</p>
