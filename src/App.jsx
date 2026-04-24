f6bb51d7593413604680cbd6b89d7197acb3b2de
Matthew Cserhati <csmatyi1@gmail.com>
2026-04-24 16:05:38 -0700

    260424 debugging
    got rid of Chome dark mode weird look, got rid of banner at top of image cards

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <>
-      <NavBar />
+      {!location.pathname.startsWith("/image/") && <NavBar />}
       <Routes>
 
         <Route path='/' element={<Home />} />
--- src/pages/ImageDetail.jsx
+++ src/pages/ImageDetail.jsx
@@ -46,7 +46,6 @@ function ImageDetail() {
 
     return (
         <div style={{ padding: 24 }}>
-            <Link to="/explore">Back to Explore</Link>
             <h1>{item.taxon}</h1>
             <p>
                 {taxId && (
